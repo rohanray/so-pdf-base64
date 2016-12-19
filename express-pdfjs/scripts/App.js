59fb5a292e3662fefeddce634667b9d20b7fc0f5
Rohan Ray <rohanray@gmail.com>
2016-12-19 09:00:31 +0530

    clean up

--- express-pdfjs/scripts/App.js
+++ express-pdfjs/scripts/App.js
@@ -3,33 +3,6 @@ const rp = require('request-promise-native')
 require('pdfjs-dist')
 PDFJS.workerSrc = 'pdfjs/build/pdf.worker.js'
 
-function getBase64Pdf() {
-  // request('http://localhost:5000/getBase64Pdf', function (error, response, body) {
-  //   if (!error && response.statusCode == 200) {
-  //     console.log(response.statusCode) // 200
-  //     console.log(response.headers['content-type']) // 'image/png'
-  //     // console.log(body)
-  //   }
-  // })
-
-  let options = {
-    method: 'GET',
-    uri: 'http://localhost:5000/getBase64Pdf',
-    resolveWithFullResponse: true
-  }
-
-  return rp(options)
-    .then((response) => {
-      if (response.statusCode !== 200) {
-        console.error("http not 200 but : ", response.statusCode)
-        return false
-      } else {
-        console.info("connected successfully : " + response.statusCode)
-        return response
-      }
-    })
-}
-
 export default class App extends Component {
 
   constructor(props) {
@@ -42,10 +15,9 @@ export default class App extends Component {
     rp(options)
       .then((response) => {
         if (response.statusCode !== 200) {
-          console.error("http not 200 but : ", response.statusCode)
-          return false
+          console.error('http not 200 but : ', response.statusCode)
         } else {
-          console.info("connected successfully : " + response.statusCode)
+          console.info('connected successfully : ' + response.statusCode)
           // console.log('pdfData : ', response.body)
           let pdfData = atob(response.body)
           let uint8ArrayPdf = new Uint8Array(pdfData.length)
@@ -53,7 +25,7 @@ export default class App extends Component {
             uint8ArrayPdf[i] = pdfData.charCodeAt(i)
           }
           let pdfjsframe = document.getElementById('pdfViewer');
-          console.log("pdfjsframe : ",pdfjsframe.contentWindow.PDFViewerApplication)
+          // console.log("pdfjsframe : ", pdfjsframe.contentWindow.PDFViewerApplication)
           pdfjsframe.contentWindow.PDFViewerApplication.open(uint8ArrayPdf);
         }
       })
@@ -61,10 +33,7 @@ export default class App extends Component {
 
   render() {
     return (
-      // Add your component markup and other subcomponent references here.
-      <div>
-        <h1>{PDFJS.workerSrc}</h1>
-      </div>
+      <div/>
     );
   }
 }
